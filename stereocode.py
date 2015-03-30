6132ebac6acd4ff95705edbf718d5c3697b1b9e2
Brian Bartman <bbartman@kent.edu>
2015-03-30 10:19:36 -0400

    Renamed a file and attempting to get the final part of the test suite up and working.

--- stereocode.py
+++ stereocode.py
@@ -28,11 +28,58 @@ The main file that's used for executing stereocode from the command line.
     2) I/O interfacing with an archive/file/Collection of source code files/pipes
         then handing that off to srcML to be annotated/processed.
     3) Outputting to either the command line, file or archive as desired.
+
+
 """
 
 
-import srcml
-# import 
+import srcml, sys, os, os.path, logging, argparse
+from stereocode import *
+
+
+
 
 if __name__ == "__main__":
-    raise NotImplementedError("Haven't started implemented this just yet.")
+
+
+    arg_parser = argparse.ArgumentParser(
+        prog="stereocode",
+        description = 
+"""Annotate functions/methods with special different stereotypes.
+The expected input to is a srcML archive.
+
+
+This program has several methods of operation:
+    1) Source Code Re-documentation. Annotate the current source code itself with a
+        comment containing @stereotype followed by the stereotypes of that function.
+    2) XML Attribute Annotation. Each <function> is annotated with stereotype XML 
+        attribute that contains a comma separated list of stereotypes associated
+        with that method.
+    3) Function List. Creates a new archive containing an XML archive containing
+        a list of all function signatures by file paired with stereotype
+        information.
+        """
+    )
+    arg_parser.add_argument("-i,--input", metavar='N', type=int, nargs='+', help='an integer for the accumulator')
+
+    # parser.print_help()
+    # parser.add_argument('--sum', dest='accumulate', action='store_const',
+    #                    const=sum, default=max,
+    #                    help='sum the integers (default: find the max)')
+
+    args = arg_parser.parse_args()
+    # if 
+
+    # print args.accumulate(args.integers)
+
+    # sys.argv
+
+    # logging.basicConfig(level=logging.INFO)
+    # logger = logging.getLogger(__name__)
+    # logger.debug("Starting to processing arguments")
+
+    # Configuring argument parsing.
+    # 
+
+    # logger.debug("Completed argument processing")
+    #raise NotImplementedError("Haven't started implemented this just yet.")
