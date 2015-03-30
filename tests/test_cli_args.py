e4514cded20e3767550b0129e1b726de9f83f25e
Brian Bartman <bbartman@kent.edu>
2015-03-30 10:20:03 -0400

    Forgot to add 2 test files to git.

---
+++ tests/test_cli_args.py
@@ -0,0 +1,39 @@
+##
+# @file test_cli_args.py
+#
+# @copyright Copyright (C) 2013-2014 srcML, LLC. (www.srcML.org)
+# 
+# The stereocode is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# The stereocode Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with the stereocode Toolkit; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import unittest, sys, stereocode
+
+class TestStringMethods(unittest.TestCase):
+    
+    def test_thing(self):
+        raise NotImplementedError("DAMN!")
+#   def test_upper(self):
+#       self.assertEqual('foo'.upper(), 'FOO')
+
+#   def test_isupper(self):
+#       self.assertTrue('FOO'.isupper())
+#       self.assertFalse('Foo'.isupper())
+
+#   def test_split(self):
+#       s = 'hello world'
+#       self.assertEqual(s.split(), ['hello', 'world'])
+#       # check that s.split fails when the separator is not a string
+#       with self.assertRaises(TypeError):
+#           s.split(2)
+
