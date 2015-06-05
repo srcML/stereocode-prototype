b14997c2ca380847e5f95581fc23789fb8fac4d1
Brian Bartman <bbartman@kent.edu>
2015-06-05 13:00:45 -0400

    I think I have a pretty good stopping point for stereocode.

--- setup.py
+++ setup.py
@@ -1,19 +1,30 @@
 from setuptools import setup
 import sys
 
-extra = {}
+extra = { }
+
 if sys.version_info >= (3,):
     extra['use_2to3'] = True
     raise NotImplementedError("2to3 conversion has not been tested!")
 
 setup(
     name='stereocode',
-    version = '0.9',
-    description='Source code stereotypeing tool that annotates srcML-i-fied  with stereotypes.',
+    version = '1.0',
+    description='Source code stereotypeing tool that annotates srcML-i-fied with method stereotypes.',
     author='SDML',
     author_email='',
     package_dir = {'': 'src'},
     packages = ['stereocode'],
     test_suite = ''#,
     # **extra
+
+    license = "GPL",
+    keywords = "static-analysis stereotype stereocode srcML SDML",
+    url = "www.sdml.com",
+    long_description="",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+    ],
+
 )
