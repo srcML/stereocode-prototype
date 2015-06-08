c446768a7d6a996f6dac2e24221ce46b3cc0e0d4
Brian Bartman <bbartman@kent.edu>
2015-06-08 11:17:18 -0400

    Working on updating setup but I'm confused about how it's supposed to work as far as included extra files.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from setuptools import setup
 import sys
 
@@ -8,7 +9,7 @@ if sys.version_info >= (3,):
     raise NotImplementedError("2to3 conversion has not been tested!")
 
 def read_docuemntation():
-    return "\n".join(open("README.md")).readlines()
+    return "\n".join(open("README.md").readlines())
 setup(
     name='stereocode',
     version = '1.0',
@@ -17,7 +18,7 @@ setup(
     author_email='',
     package_dir = {'stereocode': 'stereocode'},
     packages = ['stereocode'],
-    test_suite = ''#,
+    test_suite = '',
     # **extra
 
     license = "GPL",
